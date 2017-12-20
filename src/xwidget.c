veshboo/emacs

commit 8d7750f464c7c5097668875daed1376a1945094e

Functions when javascript not allowed (Bug#29565)
Support some basic xwidget webkit functions when javascript blocked by
'Content-Security-Policy'.  Replace some javascript calls with webkit
native functions and remove callbacks for the javascripts.

Refactor 'xwidget-webkit-current-url', no side effect of message or
kill-ring.  No more async, directly used in bookmarking, removing URL
text property.  Add new function 'xwidget-webkit-url-message-kill' for
old behavior.

Add new key map 'f' to new function 'xwidget-webkit-forward'.

Uniquely 'rename-buffer' with page title when the page is loaded.

Enable web inspector for ns xwidget webkit.

Change xwScript (ns xwidget webkit keyboard focus related script)
injection time to document start from document end.

* lisp/xwidget.el (xwidget-webkit-uri, xwidget-webkit-title)
(xwidget-webkit-goto-history): New webkit native functions.
(xwidget-webkit-cx2-cb, xwidget-webkit-cx3-cb)
(xwidget-webkit-url-title-cb, xwidget-webkit-current-url-cb): Remove
javascript callbacks.
(xwidget-webkit-callback): Replace javascript with webkit native.  No
URL text property.  Rename buffer unique.
(xwidget-webkit-back, xwidget-webkit-reload): Replace javascript with
webkit native.
(xwidget-webkit-mode-map, xwidget-webkit-forward): New function.
(xwidget-webkit-current-url, xwidget-webkit-current-url-message-kill)
(xwidget-webkit-cx2, xwidget-webkit-cx3)
(xwidget-webkit-bookmark-make-record): Replace javascript with webkit
native.  Refactor 'xwidget-webkit-current-url'.
* src/nsxwidget.h
* src/nsxwidget.m (nsxwidget_webkit_uri, nsxwidget_webkit_title)
(nsxwidget_webkit_goto_history): New webkit native functions.
(XwWebView::urlScriptBlocked, XwWebView::initWithFrame)
(XwWebView::decidePolicyForNavigationResponse, nsxwidget_kill): Add
property mapping URL to whether javascript allowed or not.  Enable web
inspector.  Change user script injection time.
(XwWebView::keyDown): Send keyDown event to emacs when javascript not

src/xwidget.c
@@ -753,6 +753,36 @@ xwidget_is_web_view (struct xwidget *xw)
       return Qnil;							\
     }
 
+DEFUN ("xwidget-webkit-uri",
+       Fxwidget_webkit_uri, Sxwidget_webkit_uri,
+       1, 1, 0,
+       doc: /* Get the current URL of XWIDGET webkit.  */)
+  (Lisp_Object xwidget)
+{
+  WEBKIT_FN_INIT ();
+#if defined (USE_GTK)
+  WebKitWebView *wkwv = WEBKIT_WEB_VIEW (xw->widget_osr);
+  return build_string (webkit_web_view_get_uri (wkwv));
+#elif defined (NS_IMPL_COCOA)
+  return nsxwidget_webkit_uri (xw);
+#endif
+}
+
+DEFUN ("xwidget-webkit-title",
+       Fxwidget_webkit_title, Sxwidget_webkit_title,
+       1, 1, 0,
+       doc: /* Get the current title of XWIDGET webkit.  */)
+  (Lisp_Object xwidget)
+{
+  WEBKIT_FN_INIT ();
+#if defined (USE_GTK)
+  WebKitWebView *wkwv = WEBKIT_WEB_VIEW (xw->widget_osr);
+  return build_string (webkit_web_view_get_title (wkwv));
+#elif defined (NS_IMPL_COCOA)
+  return nsxwidget_webkit_title (xw);
+#endif
+}
+
 DEFUN ("xwidget-webkit-goto-uri",
        Fxwidget_webkit_goto_uri, Sxwidget_webkit_goto_uri,
        2, 2, 0,
@@ -765,6 +795,27 @@ DEFUN ("xwidget-webkit-goto-uri",
   webkit_web_view_load_uri (WEBKIT_WEB_VIEW (xw->widget_osr), SSDATA (uri));
 #elif defined (NS_IMPL_COCOA)
   nsxwidget_webkit_goto_uri (xw, SSDATA (uri));
+#endif
+  return Qnil;
+}
+
+DEFUN ("xwidget-webkit-goto-history",
+       Fxwidget_webkit_goto_history, Sxwidget_webkit_goto_history,
+       2, 2, 0,
+       doc: /* Make the XWIDGET webkit load REL-POS (-1, 0, 1) page in browse history.  */)
+  (Lisp_Object xwidget, Lisp_Object rel_pos)
+{
+  WEBKIT_FN_INIT ();
+  CHECK_RANGED_INTEGER (rel_pos, -1, 1); /* -1, 0, 1 */
+#if defined (USE_GTK)
+  WebKitWebView *wkwv = WEBKIT_WEB_VIEW (xw->widget_osr);
+  switch (XFASTINT (rel_pos)) {
+  case -1: webkit_web_view_go_back (wkwv); break;
+  case 0: webkit_web_view_go_reload (wkwv); break;
+  case 1: webkit_web_view_forward (wkwv); break;
+  }
+#elif defined (NS_IMPL_COCOA)
+  nsxwidget_webkit_goto_history (xw, XFASTINT (rel_pos));
 #endif
   return Qnil;
 }
@@ -1092,7 +1143,10 @@ syms_of_xwidget (void)
   defsubr (&Sxwidget_query_on_exit_flag);
   defsubr (&Sset_xwidget_query_on_exit_flag);
 
+  defsubr (&Sxwidget_webkit_uri);
+  defsubr (&Sxwidget_webkit_title);
   defsubr (&Sxwidget_webkit_goto_uri);
+  defsubr (&Sxwidget_webkit_goto_history);
   defsubr (&Sxwidget_webkit_zoom);
   defsubr (&Sxwidget_webkit_execute_script);
   DEFSYM (Qwebkit, "webkit");

lisp/xwidget.el
@@ -39,9 +39,14 @@
 (declare-function xwidget-buffer "xwidget.c" (xwidget))
 (declare-function xwidget-size-request "xwidget.c" (xwidget))
 (declare-function xwidget-resize "xwidget.c" (xwidget new-width new-height))
+;; @callback - Prefer defun to lambda, not to be garbage collected
+;; before its execution in `xwidget-webkit-callback'.
 (declare-function xwidget-webkit-execute-script "xwidget.c"
                   (xwidget script &optional callback))
+(declare-function xwidget-webkit-uri "xwidget.c" (xwidget))
+(declare-function xwidget-webkit-title "xwidget.c" (xwidget))
 (declare-function xwidget-webkit-goto-uri "xwidget.c" (xwidget uri))
+(declare-function xwidget-webkit-goto-history "xwidget.c" (xwidget rel-pos))
 (declare-function xwidget-webkit-zoom "xwidget.c" (xwidget factor))
 (declare-function xwidget-plist "xwidget.c" (xwidget))
 (declare-function set-xwidget-plist "xwidget.c" (xwidget plist))
@@ -97,37 +102,21 @@ Interactively, URL defaults to the string looking like a url around point."
         (xwidget-webkit-new-session url)
       (xwidget-webkit-goto-url url))))
 
-;; NOTE: @javascript-callback - prefer defun to lambda.
-;; Lambda seems to be more easily garbage collected in flight from
-;; `xwidget-webkit-execute-script' to its execution via event.
-
-;; @javascript-callback
-(defun xwidget-webkit-cx2-cb (url)
-  "New xwidget webkit session and buffer with URL in split window below."
-  (with-selected-window (split-window-below)
-    (xwidget-webkit-new-session url)))
-
-;; @javascript-callback
-(defun xwidget-webkit-cx3-cb (url)
-  "New xwidget webkit session and buffer with URL in split window right."
-  (with-selected-window (split-window-right)
-    (xwidget-webkit-new-session url)))
-
 (defun xwidget-webkit-cx2 ()
-  "Get the URL of current session, then `xwidget-webkit-cx2-cb'."
+  "Get the URL of current session, then browse to the URL \
+in `split-window-below' with a new xwidget webkit session."
   (interactive)
-  (xwidget-webkit-execute-script
-   (xwidget-webkit-current-session)
-   "document.URL"
-   'xwidget-webkit-cx2-cb))
+  (let ((url (xwidget-webkit-current-url)))
+    (with-selected-window (split-window-below)
+      (xwidget-webkit-new-session url))))
 
 (defun xwidget-webkit-cx3 ()
-  "Get the URL of current session, then `xwidget-webkit-cx3-cb'."
+  "Get the URL of current session, then browse to the URL \
+in `split-window-right' with a new xwidget webkit session."
   (interactive)
-  (xwidget-webkit-execute-script
-   (xwidget-webkit-current-session)
-   "document.URL"
-   'xwidget-webkit-cx3-cb))
+  (let ((url (xwidget-webkit-current-url)))
+    (with-selected-window (split-window-right)
+      (xwidget-webkit-new-session url))))
 
 ;;todo.
 ;; - check that the webkit support is compiled in
@@ -136,10 +125,11 @@ Interactively, URL defaults to the string looking like a url around point."
     (define-key map "g" 'xwidget-webkit-browse-url)
     (define-key map "a" 'xwidget-webkit-adjust-size-dispatch)
     (define-key map "b" 'xwidget-webkit-back)
+    (define-key map "f" 'xwidget-webkit-forward)
     (define-key map "r" 'xwidget-webkit-reload)
     (define-key map "t" (lambda () (interactive) (message "o"))) ;FIXME: ?!?
     (define-key map "\C-m" 'xwidget-webkit-insert-string)
-    (define-key map "w" 'xwidget-webkit-current-url)
+    (define-key map "w" 'xwidget-webkit-current-url-message-kill)
     (define-key map "+" 'xwidget-webkit-zoom-in)
     (define-key map "-" 'xwidget-webkit-zoom-out)
 
@@ -256,25 +246,13 @@ XWIDGET instance, XWIDGET-EVENT-TYPE depends on the originating xwidget."
       (xwidget-log
        "error: callback called for xwidget with dead buffer")
     (with-current-buffer (xwidget-buffer xwidget)
-
-      ;; @javascript-callback
-      ;; We do not change selected window due to getting to knowing
-      ;; URL and title.  And also do not adjust webkit size to window
-      ;; here, the window can be the mini-buffer window unwantedly.
-      (defun xwidget-webkit-url-title-cb (url-title)
-        "Put URL as text property and change buffer name using TITLE."
-        (let ((url (car url-title))
-              (title (car (cdr url-title))))
-          (xwidget-log "webkit finished loading: '%s' from '%s'" title url)
-          (setq buffer-read-only nil)
-          (put-text-property 2 3 'URL url)
-          (setq buffer-read-only t)
-          (rename-buffer (format "*xwidget webkit: %s *" title))))
-
       (cond ((eq xwidget-event-type 'load-changed)
-             (xwidget-webkit-execute-script
-              xwidget "[document.URL, document.title]"
-              'xwidget-webkit-url-title-cb))
+;;; We do not change selected window for the finish of loading a page.
+;;; And do not adjust webkit size to window here, the selected window
+;;; can be the mini-buffer window unwantedly.
+             (let ((title (xwidget-webkit-title xwidget)))
+               (xwidget-log "webkit finished loading: %s" title)
+               (rename-buffer (format "*xwidget webkit: %s *" title) t)))
             ((eq xwidget-event-type 'decide-policy)
              (let ((strarg  (nth 3 last-input-event)))
                (if (string-match ".*#\\(.*\\)" strarg)
@@ -313,12 +291,10 @@ If non-nil, it will use a new session.  Otherwise, it will use
 nil, consider further customization with
 `xwidget-webkit-last-session-buffer'.")
 
-;; We avoid using async `xwidget-webkit-current-url', instead use URL
-;; kept in xwidget webkit as property
 (defun xwidget-webkit-bookmark-make-record ()
   "Integrate Emacs bookmarks with the webkit xwidget."
   (nconc (bookmark-make-record-default t t)
-         `((filename . ,(get-text-property 2 'URL))
+         `((filename . ,(xwidget-webkit-current-url))
            (handler  . (lambda (bmk)
                          (browse-url
                           (bookmark-prop-get bmk 'filename)
@@ -656,27 +632,27 @@ For example, use this to display an anchor."
 (defun xwidget-webkit-back ()
   "Go back in history."
   (interactive)
-  (xwidget-webkit-execute-script (xwidget-webkit-current-session)
-                                 "history.go(-1);"))
+  (xwidget-webkit-goto-history (xwidget-webkit-current-session) -1))
+
+(defun xwidget-webkit-forward ()
+  "Go forward in history."
+  (interactive)
+  (xwidget-webkit-goto-history (xwidget-webkit-current-session) 1))
 
 (defun xwidget-webkit-reload ()
-  "Reload current url."
+  "Reload current URL."
   (interactive)
-  (xwidget-webkit-execute-script (xwidget-webkit-current-session)
-                                 "history.go(0);"))
-
-;; @javascript-callback
-(defun xwidget-webkit-current-url-cb (result)
-  "Callback for `xwidget-webkit-current-url', message and kill the RESULT."
-  (let ((url (kill-new (or result ""))))
-    (message "url: %s" url)))
+  (xwidget-webkit-goto-history (xwidget-webkit-current-session) 0))
 
 (defun xwidget-webkit-current-url ()
-  "Get the webkit url and place it on the `kill-ring'."
+  "Get the current xwidget webkit URL."
   (interactive)
-  (xwidget-webkit-execute-script
-   (xwidget-webkit-current-session)
-   "document.URL" 'xwidget-webkit-current-url-cb))
+  (xwidget-webkit-uri (xwidget-webkit-current-session)))
+
+(defun xwidget-webkit-current-url-message-kill ()
+  "Message the current xwidget webkit URL and place it on the `kill-ring'."
+  (interactive)
+  (message "url: %s" (kill-new (or (xwidget-webkit-current-url) ""))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun xwidget-webkit-get-selection (proc)

src/nsxwidget.h
@@ -32,7 +32,10 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 /* Functions for xwidget webkit.  */
 
 bool nsxwidget_is_web_view (struct xwidget *xw);
+Lisp_Object nsxwidget_webkit_uri (struct xwidget *xw);
+Lisp_Object nsxwidget_webkit_title (struct xwidget *xw);
 void nsxwidget_webkit_goto_uri (struct xwidget *xw, const char *uri);
+void nsxwidget_webkit_goto_history (struct xwidget *xw, int rel_pos);
 void nsxwidget_webkit_zoom (struct xwidget *xw, double zoom_change);
 void nsxwidget_webkit_execute_script (struct xwidget *xw, const char *script,
                                       Lisp_Object fun);

src/nsxwidget.m
@@ -61,6 +61,9 @@ void store_xwidget_js_callback_event (struct xwidget *xw,
 @interface XwWebView : WKWebView
 <WKNavigationDelegate, WKUIDelegate, WKScriptMessageHandler>
 @property struct xwidget *xw;
+/* Map url to whether javascript is blocked by
+   'Content-Security-Policy' sandbox without allow-scripts.  */
+@property(retain) NSMutableDictionary *urlScriptBlocked;
 @end
 @implementation XwWebView : WKWebView
 
@@ -72,10 +75,15 @@ void store_xwidget_js_callback_event (struct xwidget *xw,
   WKUserContentController *scriptor = [[WKUserContentController alloc] init];
   configuration.userContentController = scriptor;
 
+  /* Enable inspect element context menu item for debugging.  */
+  [configuration.preferences setValue:@YES
+                               forKey:@"developerExtrasEnabled"];
+
   self = [super initWithFrame:frame configuration:configuration];
   if (self)
     {
       self.xw = xw;
+      self.urlScriptBlocked = [[NSMutableDictionary alloc] init];
       self.navigationDelegate = self;
       self.UIDelegate = self;
       self.customUserAgent =
@@ -86,7 +94,7 @@ void store_xwidget_js_callback_event (struct xwidget *xw,
       [scriptor addUserScript:[[WKUserScript alloc]
                                 initWithSource:xwScript
                                  injectionTime:
-                                  WKUserScriptInjectionTimeAtDocumentEnd
+                                  WKUserScriptInjectionTimeAtDocumentStart
                                 forMainFrameOnly:NO]];
     }
   return self;
@@ -131,6 +139,28 @@ decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler
       /* TODO: download using NSURLxxx?  */
     }
   decisionHandler (WKNavigationResponsePolicyAllow);
+
+  self.urlScriptBlocked[navigationResponse.response.URL] =
+    [NSNumber numberWithBool:NO];
+  if ([navigationResponse.response isKindOfClass:[NSHTTPURLResponse class]])
+    {
+      NSDictionary *headers =
+        ((NSHTTPURLResponse *) navigationResponse.response).allHeaderFields;
+      NSString *value = headers[@"Content-Security-Policy"];
+      if (value)
+        {
+          /* TODO: Sloppy parsing of 'Content-Security-Policy' value.  */
+          NSRange sandbox = [value rangeOfString:@"sandbox"];
+          if (sandbox.location != NSNotFound)
+            {
+              NSRange allowScripts = [value rangeOfString:@"allow-scripts"];
+              if (allowScripts.location == NSNotFound
+                  || allowScripts.location < sandbox.location)
+                self.urlScriptBlocked[navigationResponse.response.URL] =
+                  [NSNumber numberWithBool:YES];
+            }
+        }
+    }
 }
 
 /* No additional new webview or emacs window will be created
@@ -178,10 +208,20 @@ createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration
       return;
     }
 
+  /* Emacs handles keyboard events when javascript is blocked.  */
+  if ([self.urlScriptBlocked[self.URL] boolValue])
+    {
+      [self.xw->xv->emacswindow keyDown:event];
+      return;
+    }
+
   [self evaluateJavaScript:@"xwHasFocus()"
          completionHandler:^(id result, NSError *error) {
       if (error)
-        NSLog (@"xwHasFocus: %@", error.localizedDescription);
+        {
+          NSLog (@"xwHasFocus: %@", error);
+          [self.xw->xv->emacswindow keyDown:event];
+        }
       else if (result)
         {
           NSNumber *hasFocus = result; /* __NSCFBoolean */
@@ -242,11 +282,27 @@ static NSString *xwScript;
 
 /* Xwidget webkit commands.  */
 
+static Lisp_Object build_string_with_nsstr (NSString *nsstr);
+
 bool
 nsxwidget_is_web_view (struct xwidget *xw)
 {
   return xw->xwWidget != NULL &&
     [xw->xwWidget isKindOfClass:WKWebView.class];
+}
+
+Lisp_Object
+nsxwidget_webkit_uri (struct xwidget *xw)
+{
+  XwWebView *xwWebView = (XwWebView *) xw->xwWidget;
+  return build_string_with_nsstr (xwWebView.URL.absoluteString);
+}
+
+Lisp_Object
+nsxwidget_webkit_title (struct xwidget *xw)
+{
+  XwWebView *xwWebView = (XwWebView *) xw->xwWidget;
+  return build_string_with_nsstr (xwWebView.title);
 }
 
 /* @Note ATS - Need application transport security in 'Info.plist' or
@@ -259,6 +315,17 @@ nsxwidget_webkit_goto_uri (struct xwidget *xw, const char *uri)
   NSURL *url = [NSURL URLWithString:urlString];
   NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
   [xwWebView loadRequest:urlRequest];
+}
+
+void
+nsxwidget_webkit_goto_history (struct xwidget *xw, int rel_pos)
+{
+  XwWebView *xwWebView = (XwWebView *) xw->xwWidget;
+  switch (rel_pos) {
+  case -1: [xwWebView goBack]; break;
+  case 0: [xwWebView reload]; break;
+  case 1: [xwWebView goForward]; break;
+  }
 }
 
 void
@@ -340,9 +407,14 @@ void
 nsxwidget_webkit_execute_script (struct xwidget *xw, const char *script,
                                  Lisp_Object fun)
 {
-  NSString *javascriptString = [NSString stringWithUTF8String:script];
   XwWebView *xwWebView = (XwWebView *) xw->xwWidget;
+  if ([xwWebView.urlScriptBlocked[xwWebView.URL] boolValue])
+    {
+      message ("Javascript is blocked by 'CSP: sandbox'.");
+      return;
+    }
 
+  NSString *javascriptString = [NSString stringWithUTF8String:script];
   [xwWebView evaluateJavaScript:javascriptString
               completionHandler:^(id result, NSError *error) {
       if (error)
@@ -400,6 +472,7 @@ nsxwidget_kill (struct xwidget *xw)
          killed.  I could not find other solution.  */
       nsxwidget_webkit_goto_uri (xw, "about:blank");
 
+      [((XwWebView *) xw->xwWidget).urlScriptBlocked release];
       [xw->xwWidget removeFromSuperviewWithoutNeedingDisplay];
       [xw->xwWidget release];
       [xw->xwWindow removeFromSuperviewWithoutNeedingDisplay];

commit 7ec3d92363472706a734ceb9b1bccfb82f2bbcee

Fix compile failure for GTK xwidget (Bug#29565)
Fix incorrect GTK webkit function names, those were introduced with
"8d7750f464 Functions when javascript not allowed (Bug#29565)".

* src/xwidget.c (Fxwidget_webkit_goto_history)

src/xwidget.c
@@ -811,8 +811,8 @@ DEFUN ("xwidget-webkit-goto-history",
   WebKitWebView *wkwv = WEBKIT_WEB_VIEW (xw->widget_osr);
   switch (XFASTINT (rel_pos)) {
   case -1: webkit_web_view_go_back (wkwv); break;
-  case 0: webkit_web_view_go_reload (wkwv); break;
-  case 1: webkit_web_view_forward (wkwv); break;
+  case 0: webkit_web_view_reload (wkwv); break;
+  case 1: webkit_web_view_go_forward (wkwv); break;
   }
 #elif defined (NS_IMPL_COCOA)
   nsxwidget_webkit_goto_history (xw, XFASTINT (rel_pos));